9b95e9bc233af051e941443452a7a8a3901c0766
Lenovo <Lenovo@DESKTOP-1BR19GA>
2025-03-17 08:02:47 +0200

    ..

---
+++ Request-Processing-Pipeline/FromTwentyToHundredExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace RequestProcessingPipeline
+{
+    public static class FromTwentyToHundredExtensions
+    {//метод расширения добавляет метод/ расширяет класс IApplicationBuilder но без наследования.
+        public static IApplicationBuilder UseFromTwentyToHundred(this IApplicationBuilder builder)//IApplicationBuilder этот обьект реализующий интерфейс и представляет собой middleware конвеер 
+        {
+            return builder.UseMiddleware<FromTwentyToHundredMiddleware>();//добавляем в middleware конвеер свой собственный компонент.Вызывая обощенный метод UseMiddleware и типизируя этот метод классом компонента 
+        }
+    }
+}
---
+++ Request-Processing-Pipeline/FromTwentyToHundredMiddleware.cs
@@ -0,0 +1,57 @@
+﻿namespace RequestProcessingPipeline
+{
+    public class FromTwentyToHundredMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public FromTwentyToHundredMiddleware(RequestDelegate next)//сюда приходит ссылка на следующий middleware компонент в конвеере. 
+        {
+            this._next = next;//сохраняется в классе
+        }
+        //То есть в цепочке компонентов, этот знает про следующий и может передать эстафетную палочку дальше
+        public async Task Invoke(HttpContext context)
+        {
+            string? token = context.Request.Query["number"]; // Получим число из контекста запроса
+            try
+            {
+                int number = Convert.ToInt32(token);
+                number = Math.Abs(number);
+                if (number < 20)
+                {
+                    await _next.Invoke(context); //Контекст запроса передаем следующему компоненту
+                }
+                else if(number > 100)
+                {
+                    // Выдаем окончательный ответ клиенту
+                    await context.Response.WriteAsync("Number greater than one hundred");
+                }
+                else if (number == 100)
+                {
+                    // Выдаем окончательный ответ клиенту
+                    await context.Response.WriteAsync("Your number is one hundred");
+                }
+                else
+                {
+                    string[] Tens = { "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+                    if (number % 10 == 0)
+                    {
+                        // Выдаем окончательный ответ клиенту
+                        await context.Response.WriteAsync("Your number is " + Tens[number / 10 - 2]); 
+                    }
+                    else
+                    { 
+                        await _next.Invoke(context); // Контекст запроса передаем следующему компоненту.Будет ждать пока второй компанент не передаст ему управление await отпустит поток то первый компонент делает следующую логику.
+                        string? result = string.Empty;
+                        result = context.Session.GetString("number"); // получим число от компонента FromOneToTenMiddleware
+                        // Выдаем окончательный ответ клиенту
+                        await context.Response.WriteAsync("Your number is " + Tens[number / 10 - 2] + " " + result);
+                    }                   
+                }              
+            }
+            catch (Exception)
+            {
+                // Выдаем окончательный ответ клиенту
+                await context.Response.WriteAsync("Incorrect parameter");
+            }
+        }
+    }
+}
